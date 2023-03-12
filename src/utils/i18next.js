b8cdf1dc64baf5202cdb78b25962e227b362a9d6
AlexanderDmitriev <alex9110077@gmail.com>
2023-03-12 20:16:12 +0100

    1

--- src/utils/i18next.js
+++ src/utils/i18next.js
@@ -1,25 +1,24 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
+import ua from '../locales/ua/translation.json';
+import en from '../locales/en/translation.json';
+import ru from '../locales/ru/translation.json';
 
 i18n
   .use(LanguageDetector)
-  .use(HttpApi)
   .use(initReactI18next)
   .init({
+    resources: { 'en-US': en, 'ua-UA': ua, 'ru-RU': ru },
     fallbackLng: 'ua',
     supportedLngs: ['ua', 'ru', 'en'],
-    debug: false,
+    debug: true,
     detection: {
       order: ['cookie', 'localStorage'],
       caches: ['cookie', 'localStorage'],
     },
     interpolation: {
       escapeValue: false,
-    },
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json',
     },
   });
 
