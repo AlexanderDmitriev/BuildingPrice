865983aaa38edf328159e347cab12d50bad5dec4
AlexanderDmitriev <alex9110077@gmail.com>
2023-05-03 16:49:18 +0200

    Update HomePage.styled.jsx

--- src/components/Home/HomePage.styled.jsx
+++ src/components/Home/HomePage.styled.jsx
@@ -107,6 +107,7 @@ export const AboutUsTitle = styled.h2`
   color: #191924;
   border-bottom: 1px solid #418b77;
   width: 90%;
+  text-align: center;
   @media screen and (min-width: 480px) {
     font-size: 48px;
   }
@@ -115,6 +116,7 @@ export const AboutUsTitle = styled.h2`
     line-height: 110%;
   }
   @media screen and (min-width: 1200px) {
+    text-align: start;
     font-size: 100px;
     line-height: 120%;
     /* or 120px */
@@ -171,28 +173,56 @@ export const AboutUsImg = styled.img`
 `;
 ////////////////////////////
 export const OffersSection = styled.div`
-  padding-bottom: 300px;
-  display: flex;
-  justify-content: space-between;
+  padding-bottom: 150px;
+  display: block;
+  @media screen and (min-width: 480px) {
+    padding-bottom: 200px;
+  }
+  @media screen and (min-width: 768px) {
+    padding-bottom: 250px;
+    display: flex;
+    justify-content: space-between;
+  }
+  @media screen and (min-width: 1200px) {
+    padding-bottom: 300px;
+  }
 `;
 
 export const OffersText = styled.div`
-  width: 50%;
+  width: 100%;
+  margin: 0 auto;
+  @media screen and (min-width: 768px) {
+    width: 50%;
+  }
 `;
+
 export const OffersTitle = styled.h2`
   font-family: Inter serif;
   font-style: normal;
   font-weight: 500;
-  font-size: 100px;
+  font-size: 36px;
   line-height: 120%;
   /* identical to box height, or 120px */
-  margin: 0;
-
+  margin: 0 auto;
   align-items: center;
+  position: relative;
   text-transform: uppercase;
-
+  text-align: center;
   color: #191924;
   border-bottom: 1px solid #418b77;
+  @media screen and (min-width: 480px) {
+    font-size: 48px;
+  }
+  @media screen and (min-width: 768px) {
+    font-size: 56px;
+    line-height: 110%;
+    margin: 0;
+  }
+  @media screen and (min-width: 1200px) {
+    font-size: 100px;
+    line-height: 120%;
+    /* or 120px */
+  }
   div {
     height: 24px;
     display: block;
@@ -204,19 +234,33 @@ export const OffersTopic = styled.p`
   font-family: Inter serif;
   font-style: normal;
   font-weight: 400;
-  font-size: 20px;
+  font-size: 14px;
   line-height: 110%;
   /* or 22px */
-
   display: flex;
   align-items: center;
   color: #191924;
   padding-bottom: 24px;
+  @media screen and (min-width: 480px) {
+    font-size: 16px;
+  }
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+  }
 `;
 
 export const OffersList = styled.ul`
   & li:not(:last-child) {
-    padding-bottom: 40px;
+    padding-bottom: 25px;
+    @media screen and (min-width: 480px) {
+      padding-bottom: 30px;
+    }
+    @media screen and (min-width: 768px) {
+      padding-bottom: 35px;
+    }
+    @media screen and (min-width: 1200px) {
+      padding-bottom: 40px;
+    }
   }
 `;
 
@@ -224,27 +268,49 @@ export const OfferListItem = styled.li`
   font-family: Inter serif;
   font-style: normal;
   font-weight: 300;
-  font-size: 48px;
+  font-size: 20px;
   line-height: 110%;
   /* identical to box height, or 53px */
-
   display: flex;
   align-items: center;
   text-align: right;
-
   color: #191924;
+  @media screen and (min-width: 480px) {
+    font-size: 24px;
+  }
+  @media screen and (min-width: 768px) {
+    font-size: 26px;
+  }
+  @media screen and (min-width: 1200px) {
+    font-size: 48px;
+  }
+
   &::before {
     content: '';
     display: inline-block;
+    background-size: contain;
     margin-right: 40px;
+    height: 20px;
+    width: 16px;
+    background-image: url(${pointList});
+    @media screen and (min-width: 480px) {
     height: 24px;
     width: 20px;
-    background-image: url(${pointList});
+  }
   }
 `;
 
 export const ClientsSection = styled.div`
-  padding-bottom: 200px;
+  padding-bottom: 125px;
+  @media screen and (min-width: 480px) {
+    padding-bottom: 150px;
+  }
+  @media screen and (min-width: 768px) {
+    padding-bottom: 175px;
+  }
+  @media screen and (min-width: 1200px) {
+    padding-bottom: 200px;
+  }
 `;
 
 export const ClienList = styled.ul`
@@ -261,18 +327,23 @@ export const ClientListItem = styled.li`
   font-family: Inter serif;
   font-style: normal;
   font-weight: 300;
-  font-size: 36px;
+  font-size: 20px;
   line-height: 110%;
   /* identical to box height, or 53px */
-
   margin-bottom: 10px;
   align-items: center;
   text-align: right;
-  /* background-color: #547fe7; */
-  /* border-radius: 10px; */
   border-left: 1px solid #418b77;
   border-right: 1px solid #418b77;
-
   padding: 5px 10px;
   color: #191924;
+  @media screen and (min-width: 480px) {
+    font-size: 24px;
+  }
+  @media screen and (min-width: 768px) {
+    font-size: 30px;
+  }
+  @media screen and (min-width: 1200px) {
+    font-size: 36px;
+  }
 `;
